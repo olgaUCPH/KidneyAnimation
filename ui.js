2cda8d0fe41fc78532fdff354e840b283e721a2e
nhvhr <nhhr@sund.ku.dk>
2025-11-13 11:30:16 +0100

    Update ui.js
    Updated the eventlisteners to get better functionality.

--- ui.js
+++ ui.js
@@ -36,37 +36,68 @@ export function initUI(modelVars, onReset, onReplay) {
 
     // ---- Sliders ----
     kSlider.addEventListener("mouseover", (e) => {
-        const val = parseFloat(e.target.value);
-        modelVars.k = val;  // keep real small value for model
-        kValueDisplay.textContent = (val * K_MULT).toFixed(0); // show friendly number
+        //const val = parseFloat(e.target.value);
+        //modelVars.k = val;  // keep real small value for model
+        //kValueDisplay.textContent = (val * K_MULT).toFixed(0); // show friendly number
         
         infoText.textContent = `Water permeability determines how much water is reabsorbed in the descending limb of the loop of Henle. A permeability of 100% corresponds to the normal value.`;
     });
 
+   kSlider.addEventListener("change", (e) => {
+        const val = parseFloat(e.target.value);
+        modelVars.k = val;  // keep real small value for model
+        kValueDisplay.textContent = (val * K_MULT).toFixed(0); // show friendly number
+        
+        //infoText.textContent = `Water permeability determines how much water is reabsorbed in the descending limb of the loop of Henle. A permeability of 100% corresponds to the normal value.`;
+    });
+
     maxRNaSlider.addEventListener("mouseover", (e) => {
-        modelVars.maxRNa = parseFloat(e.target.value);
-        maxRNaValueDisplay.textContent = e.target.value;
+        //modelVars.maxRNa = parseFloat(e.target.value);
+        //maxRNaValueDisplay.textContent = e.target.value;
 
         infoText.textContent = `Maximum NaCl reabsorption rate controls how much NaCl the ascending limb actively pumps into the interstitium. A maximum NaCl reabsorption rate of 100% corresponds to the normal value.`;
     });
 
+        maxRNaSlider.addEventListener("change", (e) => {
+        modelVars.maxRNa = parseFloat(e.target.value);
+        maxRNaValueDisplay.textContent = e.target.value;
+
+        //infoText.textContent = `Maximum NaCl reabsorption rate controls how much NaCl the ascending limb actively pumps into the interstitium. A maximum NaCl reabsorption rate of 100% corresponds to the normal value.`;
+    });
+
     F0Slider.addEventListener("mouseover", (e) => {
-        const val = parseFloat(e.target.value);
-        modelVars.F0 = val;  // keep real small value for model
-        F0ValueDisplay.textContent = (val * F0_MULT).toFixed(0); // show friendly number
+        //const val = parseFloat(e.target.value);
+        //modelVars.F0 = val;  // keep real small value for model
+        //F0ValueDisplay.textContent = (val * F0_MULT).toFixed(0); // show friendly number
 
         infoText.textContent = `Inflow rate sets the flow of isotonic fluid entering the descending limb of the loop of Henle. An inflow rate of 100% corresponds to the normal value.`;
     });
 
+    F0Slider.addEventListener("change", (e) => {
+        const val = parseFloat(e.target.value);
+        modelVars.F0 = val;  // keep real small value for model
+        F0ValueDisplay.textContent = (val * F0_MULT).toFixed(0); // show friendly number
+
+        //infoText.textContent = `Inflow rate sets the flow of isotonic fluid entering the descending limb of the loop of Henle. An inflow rate of 100% corresponds to the normal value.`;
+    });
+
     FvasaSlider.addEventListener("mouseover", (e) => {
+        //const val = parseFloat(e.target.value);
+        //modelVars.F0vr = val;  // keep real small value for model
+        //const displayVal = Math.round(val * F0VR_MULT / 5) * 5;
+        //FvasaValueDisplay.textContent = displayVal;
+
+        infoText.textContent = `Vasa recta inflow rate sets the blood flow entering the descending vasa recta. A vasa recta inflow rate of 100% corresponds to the normal value.`;
+    });
+
+        FvasaSlider.addEventListener("change", (e) => {
         const val = parseFloat(e.target.value);
         modelVars.F0vr = val;  // keep real small value for model
         const displayVal = Math.round(val * F0VR_MULT / 5) * 5;
         FvasaValueDisplay.textContent = displayVal;
 
-        infoText.textContent = `Vasa recta inflow rate sets the blood flow entering the descending vasa recta. A vasa recta inflow rate of 100% corresponds to the normal value.`;
+        //infoText.textContent = `Vasa recta inflow rate sets the blood flow entering the descending vasa recta. A vasa recta inflow rate of 100% corresponds to the normal value.`;
     });
-
 
     showHenle.addEventListener("mouseover", () => {
         infoText.textContent = `Countercurrent multiplication occurs in the loop of Henle.
